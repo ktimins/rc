0dc0bab40e52281bdcc48d6ad55c158849a58db9
Kyle Timins <rzitex@gmail.com>
2019-12-05 07:21:44 -0500

    Updated Update-LocalhostUI.ps1 with a user prompt option.

--- Windows/Scripts/Update-LocalhostUI.ps1
+++ Windows/Scripts/Update-LocalhostUI.ps1
@@ -1,6 +1,20 @@
 Param(
-      [Switch]$Build
-     )
+      [Switch]$Build,
+      [Switch]$Prompt
+     );
+
+If ($Prompt) {
+   $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Y`bBuild", 'Build Customer Solutions in Web/UI directory';
+   $no  = New-Object System.Management.Automation.Host.ChoiceDescription '&No Build', 'No Building of Web/UI Solutions';
+   $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no);
+   $result = $host.ui.PromptForChoice('Build Web/UI Solutions?', 'Would you like to build Web/UI Solutions?', $options, 1);
+
+   Switch ($result) {
+      0 {
+         $Build = $True;
+      }
+   }
+}
 
 Push-Location 'C:\Work\Products\DailyBuild\CommercialIntellisys\Web\UI';
 
