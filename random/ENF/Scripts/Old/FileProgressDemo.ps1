f576a2097f55f38762ab6bf9118c767cb5578f4c
Kyle Timins <KTimins@lazparking.com>
2021-03-26 12:57:58 -0400

    Removing old job stuff

--- random/ENF/Scripts/Old/FileProgressDemo.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem -Path c:\Temp
-For($i = 1; $i -le $files.count; $i++) { 
-   Write-Progress -Activity "Collecting files" -status "Finding file $i" `
-   -percentComplete ($i / $files.count*100)
-}
-$files | Select name
