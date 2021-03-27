8cfc55ad5711db3d332b979bf54bbf9436254057
Kyle Timins <rzitex@gmail.com>
2021-03-27 14:34:29 -0400

    Updated startup.cmd

--- Windows/startup.cmd
+++ Windows/startup.cmd
@@ -1,4 +1,3 @@
 start "" "C:\Windows\System32\taskmgr.exe"
-rem start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\Timinsky\Git\rc\AutoHotKey\keyboard.ahk"
-rem start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\Timinsky\Git\rc\AutoHotKey\Work.ahk"
-rem start "" "C:\Program Files (x86)\Dexpot\dexpot.exe"start "" "C:\tools\Cmder\Cmder.exe"
+start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\Timinsky\Git\rc\AutoHotKey\keyboard.ahk"
+start "" "C:\tools\Cmder\Cmder.exe"
