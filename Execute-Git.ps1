5f311266ed43889d15d2db6a3a89fd06910225ce
Kyle Timins <KTimins@lazparking.com>
2021-05-11 10:07:07 -0400

    execute-git.ps1 changes

--- Execute-Git.ps1
+++ Execute-Git.ps1
@@ -1,12 +1,17 @@
 Param(
-      [Switch]$Pull,
-      [Switch]$Push
+      [Parameter(ValueFromPipeline=$true,Position=0)]
+      [ValidateSet("Push","Pull")]
+      [String]$Run = "Push",
+      [Parameter(Position=1)]
+      [String[]]$Servers= @('origin', 'gitlab')
+
      );
 
-$servers = $('origin', 'gitlab');
 
-If ($Pull) {
-   $servers | ForEach-Object {git pull $_};
-} ElseIf ($Push) {
-   $servers | ForEach-Object {git push $_};
+"Running: 'git $($Run.ToLower())' on Servers: $($Servers -join ", ")" | Write-Output;
+
+If ($Run -eq "Pull") {
+   $Servers | ForEach-Object {git pull $_};
+} ElseIf ($Run -eq "Push") {
+   $Servers | ForEach-Object {git push $_};
 }
