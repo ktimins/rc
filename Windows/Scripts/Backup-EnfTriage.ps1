513f6766deb3ffaf2f1668daaebe9b920d0f5245
Kyle Timins <KTimins@lazparking.com>
2021-07-07 15:17:46 -0400

    removing old Insurity stuff

--- Windows/Scripts/Backup-EnfTriage.ps1
+++
@@ -1,8 +0,0 @@
-$date = Get-Date -format yyyy-MM-dd
-
-$sourceDir     = 'C:\Users\TiminsKY\Documents\ENF\ENF Triage'
-$descDir       = "\\filer02\GDrive\USERS\TiminsKy\ENF\ENF Triage"
-$descDirDate   = "$descDir\$date"
-
-Robocopy.exe $sourceDir $descDir
-Robocopy.exe $sourceDir $descDirDate
