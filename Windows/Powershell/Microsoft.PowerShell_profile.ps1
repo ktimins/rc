f8cfd5014838428e9955c82bd9f681941201eeed
Kyle Timins <KTimins@lazparking.com>
2021-09-15 09:25:56 -0400

    updating powershell profile

--- Windows/Powershell/Microsoft.PowerShell_profile.ps1
+++ Windows/Powershell/Microsoft.PowerShell_profile.ps1
@@ -291,40 +291,40 @@
       Invoke-Expression -Command:$command;
    }
 
-function Get-NTPDateTime 
-{
-   param (
-         [string] $sNTPServer = 'pool.ntp.org'
-         );
-    $StartOfEpoch=New-Object DateTime(1900,1,1,0,0,0,[DateTimeKind]::Utc);
-    [Byte[]]$NtpData = ,0 * 48;
-    $NtpData[0] = 0x1B;    # NTP Request header in first byte
-    $Socket = New-Object Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork, [Net.Sockets.SocketType]::Dgram, [Net.Sockets.ProtocolType]::Udp);
-    $Socket.Connect($sNTPServer,123);
+   function Get-NTPDateTime 
+   {
+      param (
+            [string] $sNTPServer = 'pool.ntp.org'
+            );
+       $StartOfEpoch=New-Object DateTime(1900,1,1,0,0,0,[DateTimeKind]::Utc);
+       [Byte[]]$NtpData = ,0 * 48;
+       $NtpData[0] = 0x1B;    # NTP Request header in first byte
+       $Socket = New-Object Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork, [Net.Sockets.SocketType]::Dgram, [Net.Sockets.ProtocolType]::Udp);
+       $Socket.Connect($sNTPServer,123);
+        
+       $t1 = Get-Date;    # Start of transaction... the clock is ticking...
+       [Void]$Socket.Send($NtpData);
+       [Void]$Socket.Receive($NtpData);
+       $t4 = Get-Date;    # End of transaction time
+       $Socket.Close();
+    
+       $IntPart = [BitConverter]::ToUInt32($NtpData[43..40],0);   # t3
+       $FracPart = [BitConverter]::ToUInt32($NtpData[47..44],0);
+       $t3ms = $IntPart * 1000 + ($FracPart * 1000 / 0x100000000);
+    
+       $IntPart = [BitConverter]::ToUInt32($NtpData[35..32],0);   # t2
+       $FracPart = [BitConverter]::ToUInt32($NtpData[39..36],0);
+       $t2ms = $IntPart * 1000 + ($FracPart * 1000 / 0x100000000);
+    
+       $t1ms = ([TimeZoneInfo]::ConvertTimeToUtc($t1) - $StartOfEpoch).TotalMilliseconds;
+       $t4ms = ([TimeZoneInfo]::ConvertTimeToUtc($t4) - $StartOfEpoch).TotalMilliseconds;
      
-    $t1 = Get-Date;    # Start of transaction... the clock is ticking...
-    [Void]$Socket.Send($NtpData);
-    [Void]$Socket.Receive($NtpData);
-    $t4 = Get-Date;    # End of transaction time
-    $Socket.Close();
- 
-    $IntPart = [BitConverter]::ToUInt32($NtpData[43..40],0);   # t3
-    $FracPart = [BitConverter]::ToUInt32($NtpData[47..44],0);
-    $t3ms = $IntPart * 1000 + ($FracPart * 1000 / 0x100000000);
- 
-    $IntPart = [BitConverter]::ToUInt32($NtpData[35..32],0);   # t2
-    $FracPart = [BitConverter]::ToUInt32($NtpData[39..36],0);
-    $t2ms = $IntPart * 1000 + ($FracPart * 1000 / 0x100000000);
- 
-    $t1ms = ([TimeZoneInfo]::ConvertTimeToUtc($t1) - $StartOfEpoch).TotalMilliseconds;
-    $t4ms = ([TimeZoneInfo]::ConvertTimeToUtc($t4) - $StartOfEpoch).TotalMilliseconds;
-  
-    $Offset = (($t2ms - $t1ms) + ($t3ms-$t4ms))/2;
-     
-    [String]$NTPDateTime = $StartOfEpoch.AddMilliseconds($t4ms + $Offset).ToLocalTime();
- 
-    return (Get-Date $NTPDateTime);
-}
+       $Offset = (($t2ms - $t1ms) + ($t3ms-$t4ms))/2;
+        
+       [String]$NTPDateTime = $StartOfEpoch.AddMilliseconds($t4ms + $Offset).ToLocalTime();
+    
+       return (Get-Date $NTPDateTime);
+   }
 
 # }}}
 
@@ -354,8 +354,8 @@ function Get-NTPDateTime
 # Start Up - Welcome Message {{{1
 
    Clear-Host;
-   $PSVers = "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)";
-   $welcome = $env:USERNAME + ": Welcome to Powershell v" + $PSVers + ".";
+   $PSVers = "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch) - $($PSVersionTable.PSEdition)";
+   $welcome = "$env:USERNAME: Welcome to Powershell v$PSVers.`n";
    cowthink -r $welcome | lolcat;
 
 # }}}
