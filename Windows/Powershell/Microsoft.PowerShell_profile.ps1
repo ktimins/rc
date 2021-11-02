5069b8c69a0d769c55df19cc1d0cdc6c4c5f3940
Kyle Timins <KTimins@lazparking.com>
2021-11-02 14:32:56 -0400

    Updating.

--- Windows/Powershell/Microsoft.PowerShell_profile.ps1
+++ Windows/Powershell/Microsoft.PowerShell_profile.ps1
@@ -196,6 +196,10 @@
       }
    }
 
+   Function Set-SSMSDarkMode {
+      (Get-Content 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms.pkgundef') -replace '\[\`$RootKey\`$\\Themes\\{1ded0138-47ce-435e-84ef-9ec1f439b749}\]', '//[`$RootKey`$\Themes\{1ded0138-47ce-435e-84ef-9ec1f439b749}]' | Out-File 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms.pkgundef'
+   }
+
    Function Start-CountdownTimer {
       <#
 
--- Windows/startup.cmd
+++ Windows/startup.cmd
@@ -1,3 +1,3 @@
 start "" "C:\Windows\System32\taskmgr.exe"
-rem start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\Timinsky\Git\rc\AutoHotKey\keyboard.ahk"
+start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\KTimins\Git\rc\AutoHotKey\keyboard.ahk"
 start "" "C:\tools\Cmder\Cmder.exe"
