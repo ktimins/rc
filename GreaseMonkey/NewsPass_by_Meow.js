974fc5f90eb998d2bd48bbf00b1e002bfbb31f63
Kyle Timins <KTimins@lazparking.com>
2022-10-17 15:28:34 -0400

    Adding News Pass GreaseMonkey script.

---
+++ GreaseMonkey/NewsPass_by_Meow.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         News Pass
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  Removes "you can't continue reading" popups on news articles.
+// @author       Meow
+// @match        https://*.telegraph.co.uk/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    if(window.location.href.includes("telegraph.co.uk")) {
+        setInterval(function() {
+            try {
+                let st = document.createElement('style')
+                st.innerHTML = ".martech-overlay-not-visible {overflow-y: visible !important}"
+                document.head.appendChild(st)
+                document.querySelector('.martech-modal-component-overlay').remove()
+                console.log("Removed paywall")
+            } catch(e) {}
+        })
+    }
+})()
+
+//https://greasyfork.org/en/scripts/402153-news-pass
