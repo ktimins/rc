2767199a4c0f0b4e8a79e015b582bbf66db4212a
Kyle Timins <rzitex@gmail.com>
2023-03-21 19:46:05 -0400

    startup.cmd

--- Windows/startup.cmd
+++ Windows/startup.cmd
@@ -1,3 +1,3 @@
 start "" "C:\Windows\System32\taskmgr.exe"
-start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\KTimins\Git\rc\AutoHotKey\keyboard.ahk"
+start "" "C:\ProgramData\chocolatey\bin\AutoHotkey.exe" "C:\Users\rzite\Git\rc\AutoHotKey\keyboard.ahk"
 start "" "C:\tools\Cmder\Cmder.exe"
